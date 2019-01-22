1f511ebf3ffc83a0fbaaba144cad929a05ec8fa2
Thomas Tursics <thomas@tursics.de>
2019-01-22 08:27:15 +0100

    init social media stuff

--- index.js
+++ index.js
@@ -686,7 +686,7 @@ $(document).on("pageshow", "#pageMap", function () {
 		var size = $('#selectEmbedSize').val().split('x'),
 			x = size[0],
 			y = size[1],
-			html = '<iframe src="https://tursics.github.io/schulsanierung/karte-quereinsteiger.html" width="' + x + '" height="' + y + '" frameborder="0" style="border:0" allowfullscreen></iframe>';
+			html = '<iframe src="https://tursics.github.io/schule-quereinsteiger/" width="' + x + '" height="' + y + '" frameborder="0" style="border:0" allowfullscreen></iframe>';
 
 		$('#inputEmbedURI').val(html);
 		if (-1 === $('#embedMap iframe')[0].outerHTML.indexOf('width="' + x + '"')) {
